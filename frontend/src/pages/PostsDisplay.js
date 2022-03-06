d09914c7c47bca05f9f68fc9028748f067a61b43
EvilGru52 <drgru854@gmail.com>
2022-03-06 21:47:34 +0530

    Added background for posts display

---
+++ frontend/src/pages/PostsDisplay.js
@@ -0,0 +1,23 @@
+import ImageCard from "../components/ImageCard";
+import { postDetails } from "../Others/Data";
+function PostsDisplay() {
+  return (
+    <div
+      className="container-fluid d-flex flex-column align-items-center"
+      style={{
+        padding: "0px",
+        background: "url('https://wallpapercave.com/wp/wp5811210.jpg')",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+        backgroundPositionX: "left",
+      }}
+    >
+      {postDetails.map((value) => {
+        return <ImageCard />;
+      })}
+    </div>
+  );
+}
+
+export default PostsDisplay;
