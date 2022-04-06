d0058e2cb98a97e30f2e665dc50ee93b866ce8d7
EvilGru52 <drgru854@gmail.com>
2022-04-06 15:48:47 +0530

    Added Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
 import PostsDisplay from "./pages/PostsDisplay";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <PostsDisplay />
+      <Login />
     </>
   );
 }
